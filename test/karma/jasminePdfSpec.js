8857278079f040ebbf60d13bd56f74b388a0ccb2
Frank <phranck+github@gmail.com>
2016-05-31 23:12:14 -0400

    make pdf test work for multiple files

--- test/karma/jasminePdfSpec.js
+++ test/karma/jasminePdfSpec.js
@@ -5,27 +5,44 @@ var tran2csv = require('../../app/src/transactions-to-csv');
 describe("PDF Parsing to CSV", function() {
   var pdftoArray = new pdfparser.BettermentPdfArrayParser();
 
-  it("parses pdf and matches csv", function(done) {
-    for (var file in window.__karma__.files) {  // Hack to iterate over served files.
-      if(file.endsWith('pdf')) {
-        (function(pdfFile) {  // Need closure to capture file variable.
-      	  pdfToTextArray(pdfFile).then(function(lines) {          
-            // Actual
-            var transactions = pdftoArray.parse(lines);
-            var csvBlob = tran2csv.TransactionsToCsv(transactions);
-            var actualCsvResults = blobToString(csvBlob);
+  for (var file in window.__karma__.files) {  // Hack to iterate over served files.
+    if(file.endsWith('pdf')) {
+      (function(testFile) {  // Need closure over test
+        it("Correctly parses pdf " + testFile, function(done) {
+          (function(pdfFile) {  // Need closure to capture file variable.
+            pdfToTextArray(pdfFile).then(function(lines) {
+              console.log('Parsing:', pdfFile);
 
-            // Expected
-            var expectedCsvUri = pdfFile + '.csv';
-            var expectedCsvResults = urlToString(expectedCsvUri);
+              console.log('Got lines:', lines);
+              for (var i = 0; i < lines.length; i++) {
+                console.log(lines[i]);
+              }
 
-            expect(actualCsvResults).toBe(expectedCsvResults);
-      	    done();
-          });
-        })(file);
-      }
+              // Actual
+              var transactions = pdftoArray.parse(lines);
+
+              for (var i = 0; i < transactions.length; i++) {
+                console.log(transactions[i]);
+              }
+
+              //console.log('Got transactions', transactions);
+
+              var csvBlob = tran2csv.TransactionsToCsv(transactions);
+
+              var actualCsvResults = blobToString(csvBlob);
+
+              // Expected
+              var expectedCsvUri = pdfFile + '.csv';
+              var expectedCsvResults = urlToString(expectedCsvUri);
+
+              expect(actualCsvResults).toBe(expectedCsvResults);
+              done();
+            });
+          })(testFile);
+        });
+      })(file);
     }
-  });
+  }
 });
 
 function blobToString(b) {
