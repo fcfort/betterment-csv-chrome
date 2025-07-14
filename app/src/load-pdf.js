1010a269ca2fd97acbfec7b22270daa8008db882
Frank Fort <frank.c.fort@gmail.com>
2025-07-14 05:09:45 +0000

    Update to manifest v3

--- app/src/load-pdf.js
+++ app/src/load-pdf.js
@@ -1,3 +1,3 @@
 // Need this to specify path to static pdf.worker.js.
 PDFJS = {};
-PDFJS.workerSrc = chrome.extension.getURL("pdf.worker.js");
+PDFJS.workerSrc = chrome.runtime.getURL("pdf.worker.js");
